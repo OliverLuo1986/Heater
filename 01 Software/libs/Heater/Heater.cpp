2c8152c8d95f2ab4d77783661debd7640d39c3f2
18923821218 <luotong@imagintech.com.cn>
2022-08-21 11:43:18 +0800

    fix bug

--- 01 Software/libs/Heater/Heater.cpp
+++ 01 Software/libs/Heater/Heater.cpp
@@ -3,34 +3,53 @@
 Heater::Heater(){
 	callBack = NULL;
 	targetTemp = INVALID_TEMP;
-	pwmPin = -1;
+	heat_pwm_pin = -1;
+	fan_pwm_pin = -1;
 	
 	Kp = 1;
 	Ki = 0;
 	Kd = 0;
 	
+	diff_sum = 0;
+	
 	inited = false;
 }
 
-bool Heater::begin(){
+bool Heater::init(){
 	if(callBack == NULL)
 		return false;
 	
 	if(targetTemp == INVALID_TEMP)
 		return false;
 	
-	if(pwmPin == -1)
+	if(heat_pwm_pin == -1 || fan_pwm_pin == -1)
 		return false;
 	
-	ledcSetup(0, 100, 10); //通道0， 35KHz，10位解析度
-	ledcAttachPin(pwmPin, 0); //pin14定义为通道0的输出引脚  	
+	ledcSetup(0, 30000, 10); //通道0， 30KHz，10位解析度
+	ledcAttachPin(heat_pwm_pin, 0); //pin14定义为通道0的输出引脚  	
+	
+	ledcWrite(0,1023);
+	
+	
+	//ledcSetup(1, 30000, 10); //通道1， 30KHz，10位解析度
+	//ledcAttachPin(fan_pwm_pin, 0); //pin14定义为通道0的输出引脚  	
+	//ledcWrite(1,0);	
 	
 	inited = true;
 	return true;
 }
 
+void Heater::begin(){
+	power = true;
+	diff_sum = 0;
+	last_diff = targetTemp-callBack();
+}
+
 void Heater::setTargetTemp(float temp){
 	targetTemp = temp;
+	temp_buf = temp;
+	
+	//diff_sum = 0;
 }
 
 void Heater::setKp(float kp){
@@ -38,62 +57,112 @@ void Heater::setKp(float kp){
 }
 
 void Heater::setKi(float ki){
-	Kp = ki;
+	Ki = ki;
 }
 
 void Heater::setKd(float kd){
-	Kp = kd;
+	Kd = kd;
 }
 
-void Heater::setPWMpin(int pin){
-	pwmPin = pin;
+void Heater::setPWMpin(int heat_pin, int fan_pin){
+	heat_pwm_pin = heat_pin;
+	fan_pwm_pin = fan_pin;
 }
 
+void Heater::temp_set(){
+}
+
+void Heater::fan(){
+	
+}
+
+void Heater::fan_start(){
+	fan_power = true;
+	digitalWrite(fan_pwm_pin, HIGH);
+}
+
+void Heater::fan_stop(){
+	fan_power = false;
+	digitalWrite(fan_pwm_pin, LOW);
+}
+
+void Heater::end(){
+	power = false;
+	
+	ledcWrite(0, 1023);
+}
 
 
 void Heater::setGetTempCallback(getTempCallback cb){
 	callBack = cb;
 }
 
+float Heater::getCurrentTemp(){
+	return currentTemp;
+}
+
 void Heater::handle(){
-	float diff;
+	float diff, diff_diff;
 	
 	if(inited == false){
 		return; 
 	}
 	
 	currentTemp = callBack();
+	now_temp = currentTemp;
+	targetTemp = temp_buf;
 	diff = targetTemp - currentTemp;
 
-	//printf("%.2f\n", currentTemp);
-	if(diff > 0){
-		pwmValue = Kp * diff;
-		
-		if(pwmValue > 1023)
-			pwmValue = 1023;
-		
-
-		if(PWM_ON_LEVEL == LOW)
-			pwmValue = 1023-pwmValue;
-		
+	if(power)
+	{
+		diff_sum += diff;
+		diff_diff = diff-last_diff;
+		last_diff = diff;
+		//if(diff > 0)
+		if(1)
+		{
+			pwmValue = Kp*diff + Ki*diff_sum + Kd*diff_diff;
+			
 #if 0 //HEATER_DEBUG		
-		printf("pwmValue:%d\n", pwmValue);
-#endif		
+			printf("Kp:%.2f Ki:%.2f Kd:%.2f diff_sum:%.2f diff_diff:%.2f pwmValue:%d \n", 
+					Kp, Ki, Kd, diff_sum, diff_diff, pwmValue);
+#endif			
+			
+			if(pwmValue > 1023)
+				pwmValue = 1023;
+			
+			if(pwmValue <= 0)
+				pwmValue = 0;
+			
+
+			if(PWM_ON_LEVEL == LOW)
+				pwmValue = 1023-pwmValue;
+			
+			
+		}
 		
+		else{
+			if(PWM_ON_LEVEL == LOW)
+				pwmValue = 1023;
+			else
+				pwmValue = 0;
+		}
 		
+		ledcWrite(0, pwmValue);
 	}
-	else{
-		if(PWM_ON_LEVEL == LOW)
-			pwmValue = 1023;
-		else
-			pwmValue = 0;
+	else
+	{
+		ledcWrite(0, 1023);
 	}
-	
-	ledcWrite(0, pwmValue);
 	
 #if HEATER_DEBUG	
-	printf("targetTemp:%.2f, currentTemp:%.2f, diff:%.2f pwmValue:%d\n",
-			targetTemp, currentTemp, diff,pwmValue);
+#if 0
+	printf("targetTemp:%.2f, currentTemp:%.2f, diff:%.2f diff_sum:%.2f pwmValue:%d\n",
+			targetTemp, currentTemp, diff, diff_sum, pwmValue);
+#else	
+	printf("targetTemp:%.2f, currentTemp:%.2f",
+			targetTemp, currentTemp);			
+#endif			
 #endif
 	
 };
--- 01 Software/libs/Heater/Heater.h
+++ 01 Software/libs/Heater/Heater.h
@@ -11,6 +11,24 @@
 
 #define HEATER_DEBUG		(1)
 
+#define PWM_IO 14
+#define FAN_IO 15
+
+
+
+
+
+#define OFF 0
+#define ON 1
+
+#define Re_So 0
+#define Co_Temp 1
+
+typedef enum{
+	E_TEMP_MODE1_REFLOW = 0,
+	E_TEMP_MODE2_CONST_TEMP,
+}E_TEMP_MODE;
+
 
 
 typedef float (*getTempCallback)(void) ;
@@ -26,14 +44,44 @@ class Heater{
 	void setKi(float Ki);
 	void setKd(float Kd);
 	
-	void setPWMpin(int pin);
+	void setPWMpin(int heat_pin, int fan_pin);
 	
 	void setGetTempCallback(getTempCallback cb);
 	
-	bool begin(void);
+	float getCurrentTemp();
+	
+	bool init();
+	void begin(void);
 	
 	void handle(void);
+
+#if 1	
+    
+    void end();
+    void temp_set();
+    void fan();
+	void fan_start();
+	void fan_stop();
+    bool power = OFF;
+    bool sleep_flg = OFF;
+	bool fan_power = false;
+    
+    int16_t temp_buf = 41;//设置温度
+
+    uint8_t temp_mode = 1; //0 回流 1：恒温
+
+    int16_t temp_mode1_time = 10;//恒温时间
+
+    uint8_t percent = 0;
+
+    bool temp_reached_flg = false;
+
+    uint8_t backflow_working_state = 0;
+    // int16_t backflow_temp_buf = 0;
+    int16_t backflow_temp_tmp = 0;
 	
+	int16_t now_temp=31;
+#endif	
 	private:
 	float currentTemp;
 	float targetTemp;
@@ -42,8 +90,14 @@ class Heater{
 	float Ki;
 	float Kd;
 	
-	int pwmPin;
+	float diff_sum;
+	float last_diff;
+	float diff_diff;
+	
+	int heat_pwm_pin;
 	int pwmValue;
+	
+	int fan_pwm_pin;
 	
 	bool inited;
 	
